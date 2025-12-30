63601fcc856623db2173a8b25a6c829c5b2cb467
prithvi <prithvi08242@gmail.com>
2025-12-30 12:42:51 +0530

    Day-01

---
+++ Day-01/variables.tf
@@ -0,0 +1,12 @@
+variable "ami_id" {
+    description = "This is for ami_id"
+    type = string
+    default = ""
+}
+
+variable "instance_type" {
+
+    description = "This is for ami_id"
+    type = string
+    default = ""
+}
